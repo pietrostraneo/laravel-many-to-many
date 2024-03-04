ee5bd5b4814dfecd5691fd7cb0d2acce9e7623d3
pietrostraneo <straneo_p@yahoo.it>
2024-03-04 16:10:30 +0100

    first commit

---
+++ app/Http/Controllers/Admin/TypeController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\StoreTypeRequest;
+use App\Http\Requests\UpdateTypeRequest;
+use App\Http\Controllers\Controller;
+use App\Models\Type;
+
+class TypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $types = Type::all();
+        return view('admin.types.index', compact('types'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTypeRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Type $type)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Type $type)
+    {
+        return view('admin.types.modal_edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTypeRequest  $request
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTypeRequest $request, Type $type)
+    {
+        $edit_type = $request->all();
+
+        $type->fill($edit_type);
+        $type->update();
+
+        return redirect()->route('admin.types.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Type $type)
+    {
+        //
+    }
+}
